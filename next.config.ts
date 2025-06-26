40639c482d1c169cc55ff60e8206d4af281992f6
blahmountain <bddowningjennings.dev@gmail.com>
2025-06-26 12:03:17 -0700

    setting up actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export', // static exports
+  reactStrictMode: true,
+  basePath: '/static-site',
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>butts</div>;
+  return <div>butts</div>
 }
